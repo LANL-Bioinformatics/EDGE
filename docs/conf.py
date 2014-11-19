6925479023b0612ce7ec4447356fbab8e4453b66
chienchi <chienchi@lanl.gov>
2014-11-18 22:52:06 -0700

    commit epub author

--- docs/conf.py
+++ docs/conf.py
@@ -256,3 +256,5 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
+# -- Options for epub output ----------------------------
+epub_author = 'EDGE development team'
