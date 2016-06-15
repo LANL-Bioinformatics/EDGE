a5c01fc5200c5321f5cae0f0ec870d0117d240af
chienchi <chienchi@lanl.gov>
2016-06-15 10:52:01 -0600

    use -noColorLog for the test example run which fix the output html no value issue

--- testData/runTest.sh
+++ testData/runTest.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-../runPipeline -p Ecoli_10x.1.fastq Ecoli_10x.2.fastq -c $PWD/config.txt -o $PWD/output -ref $PWD/Reference/NC_000913.fna -cpu 10 -primer $PWD/primers.fa
+../runPipeline -p Ecoli_10x.1.fastq Ecoli_10x.2.fastq -c $PWD/config.txt -o $PWD/output -ref $PWD/Reference/NC_000913.fna -cpu 10 -primer $PWD/primers.fa -noColorLog
