3b5c19f1ef5988985e8e47bc5173df186df92c74
chienchi <chienchi@lanl.gov>
2016-12-01 17:03:42 -0700

    update the grand test config and script

--- testData/runTest.sh
+++ testData/runTest.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-../runPipeline -p Ecoli_10x.1.fastq Ecoli_10x.2.fastq -c $PWD/config.txt -o $PWD/output -ref $PWD/Reference/NC_000913.fna -cpu 10 -primer $PWD/primers.fa -noColorLog
+../runPipeline -p Ecoli_10x.1.fastq Ecoli_10x.2.fastq -c $PWD/config.txt -o $PWD/output -ref $PWD/Reference/NC_000913.gbk -cpu 4 -primer $PWD/primers.fa -noColorLog
