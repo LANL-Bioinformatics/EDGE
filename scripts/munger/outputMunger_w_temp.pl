13ee15893293d2ccd5839fc84cd4a038c4a0d400
chienchi <chienchi@lanl.gov>
2016-12-06 15:55:59 -0700

    update sra download error check and display

--- scripts/munger/outputMunger_w_temp.pl
+++ scripts/munger/outputMunger_w_temp.pl
@@ -464,6 +464,7 @@ sub pull_blast {
 sub pull_sra_download {
 	my $err;
 	$err = `grep Failed $out_dir/SRA_Download/log.txt` if (-e "$out_dir/SRA_Download/log.txt");
+	$err .= `grep "ERROR" $out_dir/SRA_Download/log.txt` if (-e "$out_dir/SRA_Download/log.txt");
 	if ($err){
 		$err =~ s/(http\S+)/<a href=\"$1\"  target=\"_blank\">$1<\/a>/;
 		$vars->{SRADERR} = $err;
