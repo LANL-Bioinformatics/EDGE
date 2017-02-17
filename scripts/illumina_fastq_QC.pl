3cda17f261f42246cc5dff42aa85d42e36438364
chienchi <chienchi@lanl.gov>
2017-02-16 17:25:26 -0700

    auto detect pacbio CCS reads quality encoding with score > 41

--- scripts/illumina_fastq_QC.pl
+++ scripts/illumina_fastq_QC.pl
@@ -2203,11 +2203,12 @@ sub checkQualityFormat {
     my $l;
     my $number;
     my $offset=33;
+    my $line_num=0;
     # go thorugh the file
     my $first_line=<$fh>;
     if ($first_line !~ /^@/) {$offset=-1; return $offset;}
     OUTER:while(<$fh>){
-      
+      $line_num++;
       # if it is the line before the quality line
       if($_ =~ /^\+/){
     
@@ -2218,10 +2219,15 @@ sub checkQualityFormat {
         $number = ord($line[$i]); # get the number represented by the ascii char
       
         # check if it is sanger or illumina/solexa, based on the ASCII image at http://en.wikipedia.org/wiki/FASTQ_format#Encoding
-        if($number > 74){ # if solexa/illumina
+        if($number > 104){
+	  $offset=33;  # pacbio CCS reads
+          last OUTER;
+        }
+        elsif($number > 74){ # if solexa/illumina
           $offset=64;
           #die "This file is solexa/illumina format\n"; # print result to terminal and die
-          last OUTER; 
+          # read a few more quality line to make sure the offset 
+          last OUTER if ($line_num > 100);
         }elsif($number < 59){ # if sanger
           $offset=33;
           #die "This file is sanger format\n"; # print result to terminal and die
