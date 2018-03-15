d9fcb879cc28171a2c3b80b668eb01b632480fbe
Chien-Chi Lo <chien-chi@pn1707254.lanl.gov>
2018-03-15 14:58:51 -0600

    update FaQCs from 2.06 to 2.07

--- INSTALL.sh
+++ INSTALL.sh
@@ -59,7 +59,7 @@ echo "
 }
 
 install_FaQCs(){
-local VER=2.06
+local VER=2.07
 echo "------------------------------------------------------------------------------
                            Installing FaQCs $VER
 ------------------------------------------------------------------------------
@@ -1542,7 +1542,7 @@ fi
 if ( checkSystemInstallation FaQCs )
 then
   FaQCs_VER=`FaQCs --version 2>&1| awk '{print $2}'`;
-  if  ( echo $FaQCs_VER | awk '{if($1>="2.05") exit 0; else exit 1}' )
+  if  ( echo $FaQCs_VER | awk '{if($1>="2.06") exit 0; else exit 1}' )
   then
     echo "FaQCs $FaQCs_VER found"
   else
