e7adf8ca88dd2d6592d8d6521f84f6fc8f451641
Chienchi <chienchi@lanl.gov>
2018-04-30 14:10:02 -0600

    update PiReT tab to json script

--- scripts/tab2Json_for_dataTable.pl
+++ scripts/tab2Json_for_dataTable.pl
@@ -9,18 +9,22 @@ use FindBin qw($RealBin);
 use lib "$RealBin/../lib";
 use JSON;
 
-my $limit=1000;
+my $limit=3000;
 my $contigsizelimit=700;
 my $info;
 
 my $projdir;
 my $mode;
+my $out;
+my $delimit="tab";
 
 GetOptions(
 			"mode=s"        => \$mode,
 			"project_dir=s" => \$projdir,
 			"limit=i"	=> \$limit,
+			"delimit=s"     => \$delimit, 
 			"size=i"	=> \$contigsizelimit,
+			"out=s"         => \$out,
 			"help|?"        =>  sub{Usage()} );
 
 sub Usage{
@@ -29,17 +33,30 @@ sub Usage{
 
     Options:
           -project_dir    the project directory
-          -mode           'contig' or 'reference' or 'ref_gap' 
+          -mode           'contig' or 'reference' or 'ref_gap' or 'feature_count'
                           the first column of the tab_delimited_table 
                           is contig ID or Reference ID or other ID
+          -delimit        tab (default) or comma 
+          -limit <NUM>    process # for row for the input table. (default: 3000)
+          -out            output filename
 END
 
 exit;
 }
 
+
 my $table=$ARGV[0];
 
+my $sep = "\t"; 
+$sep = "," if ($delimit =~ /comma/i);
+
 if (!$table){&Usage();}
+
+if ($out){
+	unlink $out;
+	open (my $fh,">",$out);
+	select $fh;
+}
 
 my $projname;
 if ($projdir){
@@ -50,12 +67,14 @@ if ($projdir){
 open(my $fh, $table) or die "Cannot read $table\n";
 my $header = <$fh>;
 chomp $header;
-my @headers = split /\t/,$header;
+my @headers = split /$sep/,$header;
 splice @headers, 1, 0,  "NCBI BLAST" if ($mode eq 'contig');
 $headers[0]= "CONTIG_ID" if ($mode eq 'contig');
+shift @headers if ($mode eq "feature_count");
 my ($length_index,$start_index,$end_index);
 foreach my $i (0..$#headers){
 	my $hash;
+	$headers[$i] =~ s/^\"|\"$//g;
 	$hash->{title}= $headers[$i];
 	$hash->{data}= $headers[$i];
 	push @{$info->{columns}},$hash;
@@ -69,7 +88,7 @@ while(<$fh>){
 	my $data;
 	last if ($limit > 0 && $count >= $limit);
 
-	my @array=split(/\t/,$_);
+	my @array=split(/$sep/,$_);
 	if ($mode eq 'contig'){
 		splice @array, 1, 0,  "<a href='#' class='edge-contigBlast' >BLAST</a>";
 		my $end = ($length_index)? $array[$length_index] : $contigsizelimit;
@@ -80,8 +99,10 @@ while(<$fh>){
 		my $end = ($end_index)? $array[$end_index]: $contigsizelimit;
 		$array[0]="<a href='JBrowse/?data=data%2F$projname%2FJBrowse%2Fref_tracks&tracks=DNA%2CCDS%2CRRNA%2CTRNA&loc=$array[0]%3A$start..$end' target='_blank'>$array[0]</a>" if ($projname);
 	}
+	shift @array if ($mode eq "feature_count");
 	foreach my $i (0..$#array){
-		$array[$i] = ($array[$i] =~ /\d+\.\d+/)? sprintf("%.4f",$array[$i]):$array[$i];
+		$array[$i] =~ s/^\"|\"$//g;
+		$array[$i] = ($array[$i] !~ /\D/ && $array[$i] =~ /\d+\.\d+/)? sprintf("%.4f",$array[$i]):$array[$i];
 		$data->{$headers[$i]}=$array[$i];
 	}
 	push @{$info->{data}},$data;
