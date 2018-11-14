c4ab494a15e787138256b6e193abcd3ac0c97756
Chienchi <chienchi@lanl.gov>
2018-11-14 15:42:08 -0700

    add bioconda and conda-forge channels for installation

--- INSTALL.sh
+++ INSTALL.sh
@@ -1366,6 +1366,9 @@ tar -xvzf Anaconda3Packages.tgz
 $anaconda3bin/pip install --no-index --find-links=./Anaconda3Packages CairoSVG 
 $anaconda3bin/pip install --no-index --find-links=./Anaconda3Packages pymc3
 $anaconda3bin/pip install --no-index --find-links=./Anaconda3Packages lzstring
+$anaconda3bin/conda --add channels defaults
+$anaconda3bin/conda --add channels bioconda
+$anaconda3bin/conda --add channels conda-forge
 $anaconda3bin/conda install -c bioconda rgi=4.2.2
 ln -fs $anaconda3bin/cairosvg $rootdir/bin
 
