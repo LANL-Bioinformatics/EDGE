0669b9506f687ea5063fe5d632c893e78e75b5e9
Chienchi Lo <chien-chi@pn2034281.lanl.gov>
2021-11-19 11:00:53 -0700

    add homopolymer filter and strandBias filter for consensus workflow

--- edge_ui/javascript/edge.js
+++ edge_ui/javascript/edge.js
@@ -2179,6 +2179,8 @@ $( document ).ready(function()
 					$( '#edge-r2g-con-min-baseQ').prop('disabled',false).val(5);
 					$('#edge-r2g-con-altIndel-prop').prop('disabled',false).val('0.8').slider("refresh");
 					//$('#edge-r2g-con-disableBAQ-sw1').prop('disabled',false).click().checkboxradio("refresh");
+					//$('#edge-r2g-con-homopolymer-filter-sw1').prop('disabled',false).click().checkboxradio("refresh");
+					//$('#edge-r2g-con-strandbiase-filter-sw1').prop('disabled',false).click().checkboxradio("refresh");
 				}
 			}
 			if($('#edge-fastq-source-sw2').is(':checked')){
@@ -2201,6 +2203,8 @@ $( document ).ready(function()
 					$('#splitrim-minq').prop('disabled',false).val('20');
 					$('#edge-r2g-con-altIndel-prop').prop('disabled',false).val('0.5').slider("refresh");
 					//$('#edge-r2g-con-disableBAQ-sw2').prop('disabled',false).click().checkboxradio("refresh");
+					//$('#edge-r2g-con-homopolymer-filter-sw2').prop('disabled',false).click().checkboxradio("refresh");
+					//$('#edge-r2g-con-strandbiase-filter-sw2').prop('disabled',false).click().checkboxradio("refresh");
 				}
 			}
 		});
