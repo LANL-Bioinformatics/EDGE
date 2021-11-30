48c129efe75276a0408ec87ebe7614d872b549e9
Chienchi Lo <chien-chi@pn2034281.lanl.gov>
2021-11-29 22:24:03 -0700

    add B.1.1.529 for  VOC

--- scripts/munger/outputMunger_w_temp.pl
+++ scripts/munger/outputMunger_w_temp.pl
@@ -2156,7 +2156,7 @@ sub parse_lineage{
 			$note = "; VOC";
 			$lineage{$cid}->{warning} = "VOC";
 		}
-		if ($lineage_assign =~ /B.1.617.2/ or $lineage_assign =~ /AY./ ){
+		if ($lineage_assign =~ /B.1.617.2/ or $lineage_assign =~ /AY./ or $lineage_assign =~ /B.1.1.529/ ){
 			$note = "; VOC";
 			$lineage{$cid}->{warning} = "VOC";
 		}
