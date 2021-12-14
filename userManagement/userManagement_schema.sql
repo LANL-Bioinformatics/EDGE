ab81a585f0eaba4227b4b7567f57f296476d6a5e
Chienchi Lo <chien-chi@pn2034281.lanl.gov>
2021-12-14 14:24:14 -0700

    remove the constraint on spassword

--- userManagement/userManagement_schema.sql
+++ userManagement/userManagement_schema.sql
@@ -8,7 +8,7 @@ CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
-    spassword VARCHAR(255) NOT NULL,
+    spassword VARCHAR(255),
     first_name VARCHAR(255),
     last_name VARCHAR(255),
     type varchar(255) NOT NULL DEFAULT 'user',
