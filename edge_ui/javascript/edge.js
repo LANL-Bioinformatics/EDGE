d8ac4ba0a5ecbb79fd692b181ad0c41c31233402
Chienchi Lo <chien-chi@pn2034281.lanl.gov>
2023-05-31 15:23:39 -0600

    fix bug on updateprofile button from guest

--- edge_ui/javascript/edge.js
+++ edge_ui/javascript/edge.js
@@ -2779,7 +2779,7 @@ $( document ).ready(function()
 						return;
 					}
 				}
-				if ( localStorage.fnname != "Guest" && localStorage.lnname != "EDGE" ){
+				if ( localStorage.fnname === "Guest" && localStorage.lnname === "EDGE" ){
                                         $('#UpdateProfileBtn').hide();
                                 }
 
