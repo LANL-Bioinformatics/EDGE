6aeb38e155f8bac27ab094cd20698b5a5d4b086a
Chienchi Lo <chien-chi@pn2034281.lanl.gov>
2023-08-22 16:43:27 -0600

    add sed from conda-forge

--- INSTALL.sh
+++ INSTALL.sh
@@ -1440,7 +1440,7 @@ $mambaforgebin/conda config --add channels defaults
 $mambaforgebin/conda config --add channels bioconda
 #$mambaforgebin/conda config --add channels anaconda
 $mambaforgebin/conda config --add channels conda-forge
-$mambaforgebin/mamba install -y cython ncurses
+$mambaforgebin/mamba install -y cython ncurses sed
 $mambaforgebin/mamba install -y CairoSVG pandas kaleido mysql-connector-python biopython importlib-resources plotly matplotlib=3.5 scipy h5py
 #$mambaforgebin/pip install setuptools==58.0.4
 $mambaforgebin/pip install xlsx2csv pysam
